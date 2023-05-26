6a1cc2eb4e55d9c482e94b2b0c0e564f729c9af7
Johan Haals <johan.haals@gmail.com>
2023-05-26 15:28:50 +0200

    init
    Signed-off-by: Johan Haals <johan.haals@gmail.com>

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <textarea className="textarea" placeholder="Secret"></textarea>
+      <div className="form-control">
+        <div className="w-60">
+          <label className="label cursor-pointer">
+            <span className="label-text">One-time download</span>
+            <input type="checkbox" className="toggle" defaultChecked />
+          </label>
+
+          <label className="label cursor-pointer">
+            <span className="label-text">Generate Decryption Key</span>
+            <input type="checkbox" className="toggle" defaultChecked />
+          </label>
+        </div>
+        <button className="btn">Encrypt</button>
+      </div>
+    </main>
+  );
+}
